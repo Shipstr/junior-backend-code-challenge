9ac823c38115528a16d16563baf50b72fadb012e
Scott Willson <scott.willson@gmail.com>
2021-08-09 10:44:18 -0700

    first commit

---
+++ app/javascript/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <providers />
+  </div>
+</template>
+
+<script>
+import providers from './packs/Providers'
+
+export default {
+  components: {
+    providers: providers
+  }
+}
+</script>
+<style scoped>
+#app {
+  font-family: 'Helvetica Neue', Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
